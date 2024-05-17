8581bb4ed8ea8c6ec4620a3154aa9f03c3a9366a
Alex Sifuna <aleksifuna@gmail.com>
2024-05-18 01:13:23 +0300

    added a function that reducts a string and returns the reducted string

---
+++ 0x00-personal_data/README.md
@@ -0,0 +1,6 @@
+# 0x00. Personal data
+## Learning Objectives
+1. Examples of Personally Identifiable Information (PII)
+2. How to implement a log filter that will obfuscate PII fields
+3. How to encrypt a password and check the validity of an input password
+4. How to authenticate to a database using environment variables
---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Module contains function filter_datum
+"""
+import re
+from typing import List
+
+
+def filter_datum(fields: List[str],
+                 redaction: str,
+                 message: str,
+                 separator: str
+                 ) -> str:
+    """
+    obfuscates a message and returns it
+    """
+    for field in fields:
+        message = re.sub(f'{field}=(.*?){separator}',
+                         f'{field}={redaction}{separator}', message)
+    return message
