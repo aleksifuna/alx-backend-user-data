08c6d84f21bc4b709b7151357763644562e04d6c
Alex Sifuna <aleksifuna@gmail.com>
2024-05-20 22:09:39 +0300

    added a implemented require_auth method for auth class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -15,7 +15,14 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Checks if path requires authentication
         """
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        for link in excluded_paths:
+            if path in link:
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ Creates and return a string for the authorization header
