9eca93ae0f9bd7fcf1c98c4abee78732042df963
Alex Sifuna <aleksifuna@gmail.com>
2024-05-20 23:43:22 +0300

    added another auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Contains class defination for basic_auth
+"""
+
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    Class defination for basicAuth
+    """
