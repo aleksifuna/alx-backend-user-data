3255691793055f28d68f6a4710e8f1feb16722c7
Alex Sifuna <aleksifuna@gmail.com>
2024-05-22 01:06:42 +0300

    added a method to BasicAuth that returns the current user

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -43,9 +43,9 @@ class BasicAuth(Auth):
             return None
         try:
             decoded_string = base64.b64decode(base64_authorization_header)
-        except ValueError:
+            return decoded_string.decode('utf-8')
+        except UnicodeDecodeError:
             return None
-        return decoded_string.decode('utf-8')
 
     def extract_user_credentials(
             self,
@@ -90,3 +90,15 @@ class BasicAuth(Auth):
         if user.is_valid_password(user_pwd):
             return user
         return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Retrieves and returns User instance for a request
+        """
+        auth_header = self.authorization_header(request)
+        b64_auth_header = self.extract_base64_authorization_header(auth_header)
+        decoded_auth_header = self.decode_base64_authorization_header(b64_auth_header)
+        user_credentials = self.extract_user_credentials(decoded_auth_header)
+        user = self.user_object_from_credentials(user_credentials[0], user_credentials[1])
+        return user
+
