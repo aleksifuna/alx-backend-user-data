1e5d617e09351c0d91727741cb2c5f23346a13b5
Alex Sifuna <aleksifuna@gmail.com>
2024-05-22 13:36:27 +0300

    added a class defination of sessionauth class

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Contains SessionAuth class defination
+"""
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    SessionAuth cass defination
+    """
+    pass
