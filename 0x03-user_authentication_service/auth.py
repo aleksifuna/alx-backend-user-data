9d9ccaeac69c812a59dce9da7cb3c81c434e1fb5
Alex Sifuna <aleksifuna@gmail.com>
2024-05-28 23:30:43 +0300

    implemented a method that takes an email and creates session if availabe

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -64,3 +64,17 @@ class Auth:
                 return False
         except NoResultFound:
             return False
+
+    def create_session(self, email: str) -> str:
+        """Creates a session for user corresponding to email
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                session_id = _generate_uuid()
+                setattr(user, 'session_id', session_id)
+                self._db._session.commit()
+                return user.session_id
+            return None
+        except NoResultFound:
+            return None
