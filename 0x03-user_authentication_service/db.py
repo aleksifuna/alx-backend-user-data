434987391dcd02989a974c97544c968dcce7ab24
Alex Sifuna <aleksifuna@gmail.com>
2024-05-28 04:20:41 +0300

    added a method find_user_by to class DB that searches for User with kwargs as attribute

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,9 +1,12 @@
+#!/usr/bin/env python3
 """DB module
 """
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import NoResultFound
+from typing import Dict
 
 from user import Base
 from user import User
@@ -41,3 +44,13 @@ class DB:
         session.add(user)
         session.commit()
         return user
+
+    def find_user_by(self, **kwargs: Dict) -> User:
+        """Searches for a row matching attributes in kwargs dict
+        """
+        session = self._session
+        user = session.query(User).filter_by(**kwargs).first()
+        if user:
+            return user
+        else:
+            raise NoResultFound
