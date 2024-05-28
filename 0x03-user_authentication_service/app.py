35197d06631477103650db1875267f929798bba8
Alex Sifuna <aleksifuna@gmail.com>
2024-05-28 22:45:37 +0300

    added a function that handle user registration request

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,9 +3,10 @@
 Flask app configuration module
 """
 
-from flask import Flask, jsonify
-
+from flask import Flask, jsonify, request
+from auth import Auth
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'])
@@ -15,6 +16,28 @@ def home():
     return jsonify({
         'message': 'Bienvenue'
     })
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users():
+    """Handles user registration requests
+    """
+    email = request.form.get('email')
+    password = request.form.get('email')
+    if not email or not password:
+        return jsonify({
+            "message": "Missing attribute"
+        }), 400
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({
+            "email": user.email,
+            "message": "user created"
+        }), 200
+    except ValueError:
+        return jsonify({
+            "message": "email already registered"
+        }), 400
 
 
 if __name__ == '__main__':
